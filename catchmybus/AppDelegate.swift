220facf78c0d6361f37542441fe20e092a13a50f
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-11-11 03:49:58 +0100

    Initial Commit

---
+++ catchmybus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  catchmybus
+//
+//  Created by Kilian Koeltzsch on 11/11/14.
+//  Copyright (c) 2014 Kilian Koeltzsch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
