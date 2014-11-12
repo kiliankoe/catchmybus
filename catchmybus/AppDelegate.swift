3bed903a9aa46f80fcc811253dc449733f00cf4d
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-11-12 10:35:14 +0100

    autorefresh and fill the menu with the amount of results

--- catchmybus/AppDelegate.swift
+++ catchmybus/AppDelegate.swift
@@ -6,8 +6,9 @@
 //  Copyright (c) 2014 Kilian Koeltzsch. All rights reserved.
 //
 
-import Cocoa
+//  The term 'Bus' is used for both busses and trams in this app
 
+import Cocoa
 import Alamofire
 
 @NSApplicationMain
@@ -31,22 +32,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 		// fake a refresh when starting
 		refreshClicked(stopLabel)
+
+		var timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("updateUI"), userInfo: nil, repeats: true)
+	}
+
+	func updateUI() {
+		// let's fake this for now
+		refreshClicked(stopLabel)
 	}
 
 	@IBAction func refreshClicked(sender: NSMenuItem) {
+		println("Manual refresh button clicked")
 		let requestURL = "http://simpledvb.herokuapp.com/api/monitor/\(selectedStop)"
 		Alamofire.request(.GET, requestURL)
 			.responseJSON { (_, _, JSON, _) in
-				var resultsArray : [Dictionary<String, AnyObject>] = JSON as [Dictionary]
+				let resultsArray : [Dictionary<String, AnyObject>] = JSON as [Dictionary]
+				let firstResult = resultsArray[0] //FIXME: This sometimes throws an array out of bounds errors, why?
+				if (resultsArray.count > 0) {
 
-				let firstResult = resultsArray[0]
-				if let firstBusMinutes : NSNumber = firstResult["arrivaltime"] as? NSNumber {
-					if let firstBusDirection : String = firstResult["direction"] as? String {
-						// Setting the title twice is done on purpose to clear the necessary space
-						self.statusItem.title = firstBusMinutes.stringValue
-						self.statusItem.title = firstBusMinutes.stringValue
-						self.firstBusLabel.title = "\(firstBusDirection): \(firstBusMinutes.stringValue) Minuten"
+					// set the next bus' arrivaltime in the statusbar title
+					if let firstBusMinutes : NSNumber = firstResult["arrivaltime"] as? NSNumber {
+						if let firstBusDirection : String = firstResult["direction"] as? String {
+							// Setting the title twice is done on purpose to clear the necessary space
+							self.statusItem.title = firstBusMinutes.stringValue
+							self.statusItem.title = firstBusMinutes.stringValue
+//							self.firstBusLabel.title = "\(firstBusDirection): \(firstBusMinutes.stringValue) Minuten"
+						}
 					}
+
+					// fill the menu with the other arriving busses
+					for result in resultsArray {
+						if let resultMinutes : NSNumber = result["arrivaltime"] as? NSNumber {
+							if let resultDirection : String = result["direction"] as? String {
+								self.statusMenu.addItemWithTitle("\(resultDirection): \(resultMinutes) Minuten", action: nil, keyEquivalent: "")
+							}
+						}
+					}
+
 				}
 			}
 	}
