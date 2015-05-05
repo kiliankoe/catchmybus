749475ad6718998601db740e588db28de9dd86bf
Kilian Koeltzsch <me@kilian.io>
2015-05-05 16:11:55 +0200

    Oh no, forgot to mention stealing from @hoodie :D
    At least the name and idea^^
    
    Hallo Hendrik \o/
    Da du eh eine Benachrichtigung für diesen Commit bekommen wirst... Wie
    schaut's denn mit catchmyrust aus? :D

--- catchmybus/AppDelegate.swift
+++ catchmybus/AppDelegate.swift
@@ -78,7 +78,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
 		aboutWindowController.appURL = NSURL(string: "http://catchmybus.kilian.io")
 		aboutWindowController.appCopyright = NSAttributedString(string: "Copyright (c) 2015 Kilian Koeltzsch")
 		aboutWindowController.appEULA = NSAttributedString(string: "The MIT License (MIT)\n\nCopyright (c) 2015 Kilian Koeltzsch\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
-		aboutWindowController.appCredits = NSAttributedString(string: "Thanks for help and tipps @h4llow3En")
+		aboutWindowController.appCredits = NSAttributedString(string: "Thanks for help and tipps @h4llow3En\n\nName and idea shamelessly stolen from @hoodie")
 
 
 		// initialize timer to automatically call update() how ever often updateTime states
