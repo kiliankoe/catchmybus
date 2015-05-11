1e769d098dc63708861dedc20cf527253752f675
Kilian Koeltzsch <me@kilian.io>
2015-05-11 17:41:28 +0200

    Rename vars in Connection
    arrivalMinutes -> countdown
    arrivalDate -> date

--- catchmybus/Connection.swift
+++ catchmybus/Connection.swift
@@ -10,22 +10,24 @@ import Foundation
 
 class Connection {
 
+	let id: Int
 	let line: String
 	let direction: String
-	var arrivalMinutes: Int
-	let arrivalDate: NSDate
+	var countdown: Int
+	let date: NSDate
 
 	var selected = false
 
-	init (line: String, direction: String, arrivalMinutes: Int) {
+	init (id: Int, line: String, direction: String, countdown: Int) {
+		self.id = id
 		self.line = line
 		self.direction = direction
-		self.arrivalMinutes = arrivalMinutes
-		self.arrivalDate = NSDate(timeIntervalSinceNow: NSTimeInterval(60 * arrivalMinutes))
+		self.countdown = countdown
+		self.date = NSDate(timeIntervalSinceNow: NSTimeInterval(60 * countdown))
 	}
 
 	func update() {
-		arrivalMinutes = Int(arrivalDate.timeIntervalSinceNow) / 60
+		countdown = Int(date.timeIntervalSinceNow) / 60
 	}
 
 	func toString() -> String {
@@ -34,18 +36,18 @@ class Connection {
 		let dateformat = "%H:%M"
 		let timezone = NSTimeZone(abbreviation: "CEST")
 
-		if (arrivalMinutes > 59) {
-			let hours = arrivalMinutes / 60
-			let minutes = arrivalMinutes % 60
+		if (countdown > 59) {
+			let hours = countdown / 60
+			let minutes = countdown % 60
 			if (minutes == 0) {
-				return "\(line) \(direction): \(hours) Stunden - \(arrivalDate.dateWithCalendarFormat(dateformat, timeZone: timezone))"
+				return "\(line) \(direction): \(hours) Stunden - \(date.dateWithCalendarFormat(dateformat, timeZone: timezone))"
 			} else {
-				return "\(line) \(direction): \(hours)h \(minutes) Minuten - \(arrivalDate.dateWithCalendarFormat(dateformat, timeZone: timezone))"
+				return "\(line) \(direction): \(hours)h \(minutes) Minuten - \(date.dateWithCalendarFormat(dateformat, timeZone: timezone))"
 			}
-		} else if (arrivalMinutes == 0) {
-			return "\(line) \(direction): Jetzt - \(arrivalDate.dateWithCalendarFormat(dateformat, timeZone: timezone))"
+		} else if (countdown == 0) {
+			return "\(line) \(direction): Jetzt - \(date.dateWithCalendarFormat(dateformat, timeZone: timezone))"
 		} else {
-			return "\(line) \(direction): \(arrivalMinutes) Minuten - \(arrivalDate.dateWithCalendarFormat(dateformat, timeZone: timezone))"
+			return "\(line) \(direction): \(countdown) Minuten - \(date.dateWithCalendarFormat(dateformat, timeZone: timezone))"
 		}
 	}
 
